f416bce22ab73316d99cbc2915b5c879d7de7cd3
George Baldwin <george.baldwin@flutteruki.com>
2025-06-06 19:29:04 +0100

    Adding doc strings and comments to the controller methods

--- src/main/java/com/gigmoneytransferapi/spring/money_transfer/TransferController.java
+++ src/main/java/com/gigmoneytransferapi/spring/money_transfer/TransferController.java
@@ -20,8 +20,12 @@ public class TransferController {
         return ResponseEntity.ok(accounts);
     }
 
+    /** addAccount method adds an Account instance to the moneytransferdb database.
+     * If the account already exists then it throws an error */
     @PostMapping("/account")
     public ResponseEntity<String> addAccount(@RequestBody Account account) {
+
+        // checks that the account to add doesn't already exist
         if (accountRepository.existsById(account.getAccountID())) {
             System.out.println("Account already exists.");
             return ResponseEntity.badRequest().body("Account already in database");
@@ -31,8 +35,13 @@ public class TransferController {
         return ResponseEntity.ok(accountAddedToDB + " account added!");
     }
 
+    /** deleteAccount deletes an account from moneytransferdb.
+     * If the account doesn't exist then it throws a notfound error.
+     * The @PathVariable is used to input the id paramter into the url to retrieve the id from moneytransferdb*/
     @DeleteMapping("/accounts/{id}")
     public ResponseEntity<String> deleteAccount(@PathVariable Long id) {
+
+        // checks to make sure the account ID exists
         if (!accountRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
@@ -40,6 +49,8 @@ public class TransferController {
         return ResponseEntity.ok(id + " account deleted successfully.");
     }
 
+    /** transferMoney a chosen amount from one account to another.
+     * You cannot transfer an amount of 0 or an amount that is greater than the senders balance. */
     @PostMapping("/transfer")
     public ResponseEntity<String> transferMoney(@RequestBody TransferFunds request) {
 
@@ -47,6 +58,7 @@ public class TransferController {
         long toAccountID = request.getToAccountId();
         double amount = request.getAmount();
 
+        // conditional to make sure the amount being transferred is greater than 0
         if (amount <= 0) {
             return ResponseEntity.badRequest().body("Amount must be greater than 0!!");
         }
@@ -56,13 +68,17 @@ public class TransferController {
         Account toAccount = accountRepository.findById(toAccountID)
                 .orElseThrow(() -> new RuntimeException("Destination account not found"));
 
+        //conditional to make sure the sender has a balance greater than the amount to
+        // transfer to the destination account (toAccount)  */
         if (fromAccount.getBalance() < amount) {
             return ResponseEntity.badRequest().body("Insufficient funds in source account");
         }
 
+        // setting the new balance for both the to and from account
         fromAccount.setBalance(fromAccount.getBalance() - amount);
         toAccount.setBalance(toAccount.getBalance() + amount);
 
+        // Saves the new balance to the fromAccount and toAccount
         accountRepository.save(fromAccount);
         accountRepository.save(toAccount);
         System.out.println(amount + "has been sent to account " + toAccountID + " from account " + fromAccountID);
